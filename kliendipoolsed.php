6bf9254912db003912ed48b9b9dc27d48b077a8b
Benruuben <benruuben@gmail.com>
2015-09-23 14:16:10 +0300

    6. jQuery kass koeraks tagasi.

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -3,21 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title>Bennu</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 </head>
 <body>
 <button type="button" onclick="alert('Tere maailm')">Tere maailm!</button>
 <a href="http://www.khk.ee" onclick="alert('Tere maailm')">Tere maailm!</a>
 <a href="" onclick="alert('Jääme siia')">Jääme siia!</a>
 <img id="myImage" onclick="changeImage()" src="https://whyweprotest.net/attachments/cat-science1-jpg.206344/" width="180" height="120">
+
 <script>
-    function changeImage() {
-        var image = document.getElementById('myImage');
-        if (image.src.match("bulbon")) {
-            image.src = "https://whyweprotest.net/attachments/cat-science1-jpg.206344/";
-        } else {
-            image.src = "https://nktva42l4am2fq6f1bf2dz43-wpengine.netdna-ssl.com/wp-content/uploads/2014/09/words.jpg";
-        }
-    }
+    $(document).ready(function() {
+        $('#myImage').click(function() {
+            $('#myImage').attr("src", "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTiOfAJ_h6V2ITLN5PcYCzLYW9z8t9qQDaMUK6vEwG1QileOlUl");
+        });
+    });
 </script>
 
 
