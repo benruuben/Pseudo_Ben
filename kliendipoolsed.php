dc42e48c35914016add102a9b8f1e596afce421f
Benruuben <benruuben@gmail.com>
2015-09-23 12:24:04 +0300

    5. Kass koeraks loomine.

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -8,6 +8,17 @@
 <button type="button" onclick="alert('Tere maailm')">Tere maailm!</button>
 <a href="http://www.khk.ee" onclick="alert('Tere maailm')">Tere maailm!</a>
 <a href="" onclick="alert('Jääme siia')">Jääme siia!</a>
+<img id="myImage" onclick="changeImage()" src="https://whyweprotest.net/attachments/cat-science1-jpg.206344/" width="180" height="120">
+<script>
+    function changeImage() {
+        var image = document.getElementById('myImage');
+        if (image.src.match("bulbon")) {
+            image.src = "https://whyweprotest.net/attachments/cat-science1-jpg.206344/";
+        } else {
+            image.src = "https://nktva42l4am2fq6f1bf2dz43-wpengine.netdna-ssl.com/wp-content/uploads/2014/09/words.jpg";
+        }
+    }
+</script>
 
 
 
