a39c8bbdb6c7f88be029dbec5eca61e492eee6fb
Benruuben <benruuben@gmail.com>
2015-10-22 09:14:24 +0300

    Stringid.

--- kliendipoolsed.php
+++ kliendipoolsed.php
@@ -1,1 +1,17 @@
-Tere maailm!
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Serverip</title>
+</head>
+<body>
+
+<?php
+$eesNimi='Ben';
+$pereNimi='Kaljuvee';
+echo $eesNimi.'&nbsp'.$pereNimi;
+$vanus=20;
+echo $eesNimi.' '.$pereNimi.' ('.$vanus.')';
+ ?>
+</body>
+</html>
